d8d0e56a7f0c883bd1f4966d53bce645af70d891
Taís Foltran Orsini <taisfoltranorsini.com>
2021-06-16 16:59:26 -0300

    added checkCollision method

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -4,19 +4,17 @@ class Player {
     this.yPlayer = 270;
   }
 
-  keyUp(e) {
-    if (e.key == "Up" || e.key == "ArrowUp") {
-      upPressed = true;
-    } else if (e.key == "Down" || e.key == "ArrowDown") {
-      downPressed = true;
+  moveUp() {
+    this.yPlayer -= 25;
+    if (player.yPlayer < 0) {
+      player.yPlayer = 0;
     }
   }
 
-  keyDown(e) {
-    if (e.key == "Up") {
-      upPressed = false;
-    } else if (e.key == "Down") {
-      downPressed = false;
+  moveDown() {
+    this.yPlayer += 25;
+    if (player.yPlayer > 550) {
+      player.yPlayer = 550;
     }
   }
 
@@ -29,10 +27,43 @@ class Player {
 }
 
 class Obstacles {
+  constructor() {
+    this.obstacleRow = 3;
+    this.obstacleColumn = 5;
+    this.obstaclePadding = 10;
+    this.obstacleOffsetTop = 30;
+    this.obstacleOffsetLeft = 30;
+  }
   drawObstacles(x, y, color) {
     ctx.fillStyle = color;
     ctx.fillRect(x, y, 10, 50);
   }
+
+  //   drawObstacles() {
+  //     // let xObstacle = col * (10 + obstaclePadding) + obstacleOffsetLeft;
+  //     // let yObstacle = row * (50 + obstaclePadding) + obstacleOffsetTop;
+  //     let obstacles = [];
+
+  //     for (let i = 0; i < this.obstacleColumn; i++) {
+  //       obstacles[i] = [];
+  //       for (let j = 0; j < this.obstacleRow; j++) {
+  //         obstacles[i][j] = { x: 0, y: 0 };
+  //       }
+  //     }
+
+  //     for (let col = 0; col < this.obstacleColumn; col++) {
+  //       for (let row = 0; row < this.obstacleRow; row++) {
+  //         let xObstacle = col * (10 + obstaclePadding) + obstacleOffsetLeft;
+  //         let yObstacle = row * (50 + obstaclePadding) + obstacleOffsetTop;
+  //         obstacles[col][row].x = xObstacle;
+  //         obstacles[col][row].y = yObstacle;
+  //         ctx.beginPath();
+  //         ctx.fillRect(xObstacle, yObstacle, 10, 50);
+  //         ctx.fillStyle = "white";
+  //         ctx.closePath();
+  //       }
+  //     }
+  //   }
 }
 
 class Ball {
@@ -40,8 +71,8 @@ class Ball {
     this.xBall = 350;
     this.yBall = 300;
     this.radius = 6;
-    this.xBallSpeed = 2;
-    this.yBallSpeed = -2;
+    this.xBallSpeed = 3;
+    this.yBallSpeed = -3;
   }
 
   drawBall() {
@@ -54,19 +85,30 @@ class Ball {
   }
 
   moveBall() {
+    this.checkCollision();
     this.xBall += this.xBallSpeed;
     this.yBall += this.yBallSpeed;
 
-    if (this.xBall > 700) {
+    if (this.xBall + this.radius > 700) {
       this.xBallSpeed *= -1;
-    } else if (this.xBall < 0) {
+    } else if (this.xBall - this.radius < 0) {
       alert("GAME OVER");
       document.location.reload();
       clearInterval(interval);
-    } else if (this.yBall > 600) {
+    } else if (this.yBall + this.radius > 600) {
       this.yBallSpeed *= -1;
-    } else if (this.yBall < 1) {
+    } else if (this.yBall - this.radius < 1) {
       this.yBallSpeed *= -1;
+    }
+  }
+
+  checkCollision() {
+    if (
+      this.xBall - this.radius < player.xPlayer + 10 &&
+      this.yBall - this.radius < player.yPlayer + 50 &&
+      this.yBall + this.radius > player.yPlayer
+    ) {
+      this.xBallSpeed *= -1;
     }
   }
 }
--- assets/scripts/pong.js
+++ assets/scripts/pong.js
@@ -1,32 +1,30 @@
 const canvas = document.getElementById("the-canvas");
 const ctx = canvas.getContext("2d");
 
-let upPressed = false;
-let downPressed = false;
+let x = 700 / 2;
+let y = 600 - 30;
+let paddleX = (600 - 50) / 2;
 
 const player = new Player();
 const ball = new Ball();
 const obstacles = new Obstacles();
 
-document.addEventListener("keydown", player.keyDown, false);
-document.addEventListener("keyup", player.keyUp, false);
-
-// document.addEventListener("keydown", (e) => {
-//   switch (e.keyCode) {
-//     case 38:
-//       paddle.moveUp();
-//       break;
-//     case 40:
-//       paddle.moveDown();
-//       break;
-//   }
-//   updateCanvas();
-// });
+document.addEventListener("keydown", (e) => {
+  switch (e.keyCode) {
+    case 38:
+      player.moveUp();
+      break;
+    case 40:
+      player.moveDown();
+      break;
+  }
+});
 
 function updateCanvas() {
   ctx.clearRect(0, 0, 700, 600);
   ball.drawBall();
   player.drawPlayer();
+  //   obstacles.drawObstacles();
 
   obstacles.drawObstacles(685, 270, "white");
   obstacles.drawObstacles(685, 210, "white");
@@ -35,18 +33,6 @@ function updateCanvas() {
   obstacles.drawObstacles(685, 330, "white");
   obstacles.drawObstacles(685, 390, "white");
   obstacles.drawObstacles(685, 450, "white");
-
-  if (upPressed) {
-    player.yPlayer -= 5;
-    if (player.yPlayer + 10 > 700) {
-      player.yPlayer = 0;
-    }
-  } else if (downPressed) {
-    player.yPlayer += 5;
-    if (player.yPlayer < 0) {
-      player.yPlayer = 0;
-    }
-  }
 }
 
 const interval = setInterval(updateCanvas, 10);
